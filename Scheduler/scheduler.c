8cc348722d74cce32e358adfd4ad110eb6b3033d
BrachiGoldberg <brachig404@gmail.com>
2024-08-11 16:36:56 +0300

    add is_most_left_empty function

--- Scheduler/scheduler.c
+++ Scheduler/scheduler.c
@@ -7,14 +7,14 @@ void scheduling_tasks(scheduler* sched) {
 
 		//first, schedule the tasks' queue for QUANTUM_QUEUE times
 		//while the queue is not empty
-		for (int time_queue = 0; is_queue_empty(sched->queue) && time_queue < QUANTUM_QUEUE;
+		for (int time_queue = 0; !is_queue_empty(sched->queue) && time_queue < QUANTUM_QUEUE;
 			time_queue++) {
 			execute_queue(sched->queue);
 		}
 
 		//if the QUANTUM_QUEUE time finished or the queue is empty,
 		//the scheduler schedule the tasks' rb_tree
-		for (int time_tree = 0; is_most_left_empty(sched->tasks_tree) && time_tree < QUANTUM_TREE;
+		for (int time_tree = 0; !is_most_left_empty(sched->tasks_tree) && time_tree < QUANTUM_TREE;
 			time_tree++) {
 			execute_tree(sched->tasks_tree);
 		}
