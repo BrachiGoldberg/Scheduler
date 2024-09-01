1a7d1aea3664ab72f7fedc811fccb8ab5e002c4e
BrachiGoldberg <brachig404@gmail.com>
2024-09-01 11:45:30 +0300

    added info log message

--- Scheduler/consts.h
+++ Scheduler/consts.h
@@ -4,5 +4,8 @@
 
 #define STANDART_SIZE_MESS 256
 
+#define INFO_MESSAGE_NEW_TASK_INSERT_TO_QUEUE(mess, task_id, exec_time) snprintf((mess), "new task number %dl with %d execution time was inserted to the real-time tasks queue", (task_id), (exec_time))
+#define INFO_MESSAGE_NEW_TASK_INSERT_TO_RB_TREE(mess, task_id, exec_time) snprintf((mess), "new task number %dl with %d execution time was inserted to the red-black tree", (task_id), (exec_time))
+
 #define INFO_MESSAGE_TASK_GET_CPU(mess, task_id, time) snprintf( (mess),"task number %dl got the CPU for %d milliseconds",(task_id) ,(time))
 #define INFO_MESSAGE_TASK_FINISHED(mess, task_id) snprintf((mess), "task number %dl finished ", (task_id))
