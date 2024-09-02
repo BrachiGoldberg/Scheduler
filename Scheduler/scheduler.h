77db6b1daee009e425ace606f0edc8b8b145385b
chayaProg <chch3257311@gmail.com>
2024-09-02 13:59:46 +0300

    create thread and initialize the structures in main

--- Scheduler/scheduler.h
+++ Scheduler/scheduler.h
@@ -3,24 +3,25 @@
 
 #include "rb_tree.h"
 #include "RealTimeTaskQueue.h"
-
 #include "QueueNode.h"
-#include "RealTimeTask.c"
-
 #include "general_settings.h"
 #include "round_robin.h"
 #include "cfs.h"
-
 #include "logger.h"
 #include "consts.h"
-
-typedef struct 
+#include <windows.h>
+#include <stdio.h>
+#include <math.h>
+typedef struct
 {
-	rb_tree* tasks_tree;
-	struct real_time_task_queue* queue;
-}scheduler;
+    rb_tree* tasks_tree;
+    struct real_time_task_queue* queue;
+} scheduler;
 
 void new_task_arrival(int nice, double execution_time, scheduler* sched_point);
-
 void scheduling_tasks(scheduler* sched);
-#endif // !SCHEDULER_H
+
+DWORD WINAPI input_thread(LPVOID arg);
+DWORD WINAPI task_thread(LPVOID arg);
+
+#endif // SCHEDULER_H
