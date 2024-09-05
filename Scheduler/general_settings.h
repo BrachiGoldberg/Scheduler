18263551ee4f709ed07fc5b34a8ececc710910cf
chayaProg <chch3257311@gmail.com>
2024-09-05 13:30:56 +0300

    change const value and names

--- Scheduler/general_settings.h
+++ Scheduler/general_settings.h
@@ -1,19 +1,18 @@
 #ifndef GENERAL_SETTINGS_H
 #define GENERAL_SETTINGS_H
 
-#define SCHED_LATENCY 200000
-#define MIN_QUANTUM 5
+#define SCHED_LATENCY 20      // Latency  in milliseconds
+#define MIN_QUANTUM 5         // Minimum quantum time in milliseconds
 
+#define QUANTUM_TREE 3        // Quantum-The number of tasks  in the tree structure 
+#define QUANTUM_QUEUE 5       // Quantum-The number of tasks  in the queue 
 
-#define QUANTUM_TREE 3
-#define QUANTUM_QUEUE 5
+#define DEFULT_WEIGHT 1024    // Default task weight (neutral priority)
 
-#define DEFULT_WEIGHT 1024
-
-#define MIN_TIME_SLICE 1
+#define MIN_TIME_SLICE 1      // Minimum time slice in milliseconds
 
 #define LOGGER_FILE "log.log"
-#define MAX_LOGGER_FILE_SIZE (1024 * 1024)
+#define MAX_LOGGER_FILE_SIZE (1024 * 1024) // 1MB
 #define MAX_LOGGER_BUCKUP_FILES 20
 
 #endif // !GENERAL_SETTINGS_H
