201fb36db32497e6a2ce0cc69def7d88878f3fa4
BrachiGoldberg <brachig404@gmail.com>
2024-09-08 17:03:45 +0300

    change some contst values and after deletion the pointers are NULL

--- Scheduler/general_settings.h
+++ Scheduler/general_settings.h
@@ -3,14 +3,14 @@
 
 static long ID = 0;
 #define SCHED_LATENCY 20      // Latency  in milliseconds
-#define MIN_QUANTUM 5         // Minimum quantum time in milliseconds
+#define MIN_QUANTUM 30         // Minimum quantum time in milliseconds
 
 #define QUANTUM_TREE 3        // Quantum-The number of tasks  in the tree structure 
 #define QUANTUM_QUEUE 5       // Quantum-The number of tasks  in the queue 
 
 #define DEFULT_WEIGHT 1024    // Default task weight (neutral priority)
 
-#define MIN_TIME_SLICE 1      // Minimum time slice in milliseconds
+#define MIN_TIME_SLICE 10      // Minimum time slice in milliseconds
 
 #define LOGGER_FILE "log.log"
 #define MAX_LOGGER_FILE_SIZE (1024 * 1024) // 1MB
