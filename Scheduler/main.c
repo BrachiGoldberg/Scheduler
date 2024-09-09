4d3eb8cced5cc8b1a0238dcf91c3a0390a0348fb
BrachiGoldberg <brachig404@gmail.com>
2024-09-09 12:10:02 +0300

    add test

--- Scheduler/main.c
+++ Scheduler/main.c
@@ -9,28 +9,15 @@
 #include "general_settings.h"
 #include "consts_logs.h"
 
-void create_input_thread(HANDLE* thread_name, scheduler* sched);
-void create_task_thread(HANDLE* thread_name, scheduler* sched);
 
 //int main() {
 //	scheduler sched;
 //
-//	// Initialize the scheduler and other necessary components
-//	int success = logger_initFileLogger(LOGGER_FILE, MAX_LOGGER_FILE_SIZE, MAX_LOGGER_BUCKUP_FILES);
-//	if (success == 0) {
-//		printf(ERROR_MESSAGE_LOGGER_INIT_FAILED);
-//		exit(EXIT_FAILURE);
-//	}
-//	logger_setLevel(LogLevel_TRACE);
-//
-//	sched.tasks_tree = initial_rb_tree();
-//	sched.queue = initialize_queue();
-//
-//	create_tree_mutex();
-//	create_queue_mutex();
+//	//Initialize the scheduler and other necessary components
+//	initial_all_structs(&sched);
 //
 //	HANDLE input_thread_handle, task_thread_handle;
-//	
+//
 //	//Create the input thread
 //	create_input_thread(&input_thread_handle, &sched);
 //
@@ -54,20 +41,3 @@ void create_task_thread(HANDLE* thread_name, scheduler* sched);
 //	return 0;
 //}
 
-void create_input_thread(HANDLE* thread_name, scheduler* sched) {
-	*thread_name = CreateThread(NULL, 0, input_thread, sched, 0, NULL);
-
-	if (thread_name == NULL) {
-		LOG_ERROR(ERROR_MESSAGE_CREATE_INPUT_TREAD_FAILED);
-		return EXIT_FAILURE;
-	}
-}
-
-void create_task_thread(HANDLE* thread_name, scheduler* sched) {
-	*thread_name = CreateThread(NULL, 0, task_thread, sched, 0, NULL);
-
-	if (thread_name == NULL) {
-		LOG_ERROR(ERROR_MESSAGE_CREATE_TASK_TREAD_FAILED);
-		return EXIT_FAILURE;
-	}
-}
--- Scheduler/scheduler.c
+++ Scheduler/scheduler.c
@@ -77,3 +77,37 @@ DWORD WINAPI task_thread(LPVOID param) {
 
 	return 0;
 }
+
+void initial_all_structs(scheduler* sched) {
+
+	int success = logger_initFileLogger(LOGGER_FILE, MAX_LOGGER_FILE_SIZE, MAX_LOGGER_BUCKUP_FILES);
+	if (success == 0) {
+		printf(ERROR_MESSAGE_LOGGER_INIT_FAILED);
+		exit(EXIT_FAILURE);
+	}
+	logger_setLevel(LogLevel_TRACE);
+
+	sched->tasks_tree = initial_rb_tree();
+	sched->queue = initialize_queue();
+
+	create_tree_mutex();
+	create_queue_mutex();
+}
+
+void create_input_thread(HANDLE* thread_name, scheduler* sched) {
+	*thread_name = CreateThread(NULL, 0, input_thread, sched, 0, NULL);
+
+	if (thread_name == NULL) {
+		LOG_ERROR(ERROR_MESSAGE_CREATE_INPUT_TREAD_FAILED);
+		return EXIT_FAILURE;
+	}
+}
+
+void create_task_thread(HANDLE* thread_name, scheduler* sched) {
+	*thread_name = CreateThread(NULL, 0, task_thread, sched, 0, NULL);
+
+	if (thread_name == NULL) {
+		LOG_ERROR(ERROR_MESSAGE_CREATE_TASK_TREAD_FAILED);
+		return EXIT_FAILURE;
+	}
+}
