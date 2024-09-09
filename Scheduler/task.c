d688863e1c3f3f89fe1691499eab44cbeeb8d618
BrachiGoldberg <brachig404@gmail.com>
2024-09-09 09:44:25 +0300

    increment ID in creating new task before the assignement

--- Scheduler/task.c
+++ Scheduler/task.c
@@ -8,7 +8,7 @@ task* create_task(int nice_input, double execution_time_input, long double weigh
 		exit(EXIT_FAILURE);
 	}
 
-	new_task->id = ID++;
+	new_task->id = ++ID;
 
 	new_task->execution_time = 0;
 	new_task->remaining_time = execution_time_input;
