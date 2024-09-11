b7be40fc49e951ae8e61bd39d765c1ecef39fb84
BrachiGoldberg <brachig404@gmail.com>
2024-09-11 13:19:57 +0300

    arange the tests

--- SchedulerUnitTestProject/input_output.py
+++ SchedulerUnitTestProject/input_output.py
@@ -2,16 +2,13 @@ import time
 import random
 
 
-def open_input_file(file_name, process, number_lines=-1):
+def open_input_file(file_name, process):
     # open the input file
     with open(file_name, "r") as file:
 
         while True:
             num_lines = random.randint(1, 5) * 2
 
-            # if to read specific amount of lines from the file
-            if number_lines != -1:
-                num_lines = number_lines
             lines = [file.readline() for _ in range(num_lines)]
 
             if not any(lines):
--- SchedulerUnitTestProject/test.py
+++ SchedulerUnitTestProject/test.py
@@ -6,6 +6,7 @@ import time
 import os
 import shutil
 
+
 class MyTestCase(unittest.TestCase):
     process_path = r"C:\Users\user1\Documents\ברכי\תיכנות\קמאטק - בודקמפ\scheduler-project\Scheduler\x64\Debug\Scheduler.exe"
     file_name = ["input.txt"]
@@ -42,29 +43,16 @@ class MyTestCase(unittest.TestCase):
 
     def test_all_tasks_begun(self):
         # input_all_tasks_begun.txt file:
-        # 10 //weight = 109.95116277760000
-        # 100
-        # 8 //weight = 171.79869184000000
-        # 150
-        # -30 //weight = 827180.61255302757
-        # 40
-        # 12 //weight = 70.368744177663999
-        # 120
-        # -40 //weight = 7703719.7775489436
-        # 200
-        # total weight (not real time): 352.11859879526401
-        # total weight (real time): 8530900.3901019711
-        # min time slice of not real time task: 8, max: 20
         # min quantum of real time task: 20, max: 20
         # max time wait for all tasks need to begin: 20*5 = 100 milliseconds
         # min time wait for all tasks need to begin: 8*3+20*2 = 64 milliseconds
 
-        file_name = r"input_all_tasks_begun.txt"
-        number_lines = 10
+        file_name = r"inputs/input_all_tasks_begun.txt"
+        # number_lines = 10
         time_to_wait = 60  # in milliseconds
 
         self.create_process()
-        input_output.open_input_file(file_name, self.process, number_lines)
+        input_output.open_input_file(file_name, self.process)
 
         time.sleep(time_to_wait/1000)
         self.process.stdin.close()
@@ -88,12 +76,12 @@ class MyTestCase(unittest.TestCase):
         # there are 9 real time tasks and after them 1 not real time task
         # the min time the last task get the CPU is 5*20
 
-        file_name = r"input_many_real_time_tasks_task_entered_and_got_CPU.txt"
-        number_lines = 20
+        file_name = r"inputs/input_many_real_time_tasks_task_entered_and_got_CPU.txt"
+        # number_lines = 20
         time_to_wait = 100  # in milliseconds
 
         self.create_process()
-        input_output.open_input_file(file_name, self.process, number_lines)
+        input_output.open_input_file(file_name, self.process)
 
         time.sleep(time_to_wait / 1000)
         self.process.stdin.close()
